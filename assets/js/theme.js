823312e6df0aa3104b3c43e52c853f49ced821bf
Ali Jabbari <85466832+aliejabbari@users.noreply.github.com>
2022-10-07 01:31:13 +0330

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -4,9 +4,4 @@ const themeMap = {
     'text-color': 'white',
     'highlight-color': '#2e2e2e'
   }
-  'dark': {
-    'background-color': '222',
-    'text-color': 'white',
-    'highlight-color': '#2e2e2e'
-}
 }
